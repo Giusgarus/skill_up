5f68ad67f5aed1c7ffe2acb6f1aff026adc9af6a
Gianluca <gianluca.panzani97@gmail.com>
2025-11-23 20:56:38 +0100

    LLM interaction + payload classes + .env file + gathering service created

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ from backend.services.authentication.server import router as authentication_rout
 from backend.services.challenges.server import router as challenges_router
 from backend.services.gamification.server import router as gamification_router
 from backend.services.notifications.server import router as notifications_router
+from backend.services.gathering.server import router as gathering_router
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -34,6 +35,7 @@ app.include_router(authentication_router)
 app.include_router(challenges_router)
 app.include_router(gamification_router)
 app.include_router(notifications_router)
+app.include_router(gathering_router)
 
 if __name__ == "__main__":
     import uvicorn
